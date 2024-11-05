ca7ca2f7cbe52501171a46d4abe15a95e36729a1
Enriquehub12 <isic24.jsanchezt@itesco.edu.mx>
2024-11-04 20:42:58 -0600

    cambios

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
 let menuVisible = false;
 //Función que oculta o muestra el menu
-function mostrarOcultarMenu(){
-    if(menuVisible){
-        document.getElementById("nav").classList ="";
+function mostrarOcultarMenu() {
+    if (menuVisible) {
+        document.getElementById("nav").classList = "";
         menuVisible = false;
-    }else{
-        document.getElementById("nav").classList ="responsive";
+    } else {
+        document.getElementById("nav").classList = "responsive";
         menuVisible = true;
     }
 }
 
-function seleccionar(){
+function seleccionar() {
     //oculto el menu una vez que selecciono una opcion
     document.getElementById("nav").classList = "";
     menuVisible = false;
 }
+function limpiarlabels() {
+    // Selecciona los elementos label por su id y cambia su contenido
+    document.getElementById('label1').value = '';
+    document.getElementById('label2').value = '';
+    document.getElementById('label3').value= '';
+    document.getElementById('label4').value= '';
+    document.getElementById('label5').value= '';
 
+}
